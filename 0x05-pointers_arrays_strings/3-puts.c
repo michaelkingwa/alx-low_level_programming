8333b56a158749755bf0e647efa2bb146b9ff28e
michaelkingwa <kingwamaureen@gmail.com>
2023-07-24 07:25:36 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - Prtints a string
+ * @str: The string to print
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
