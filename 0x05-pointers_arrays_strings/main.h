17752ff4f451270f69b7f25ed849ba7c3be3d4b4
michaelkingwa <kingwamaureen@gmail.com>
2023-07-24 19:59:25 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <unistd.h>
-
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
@@ -24,6 +18,4 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src)
 
-int _atoi(char *s);
 
-#endif
