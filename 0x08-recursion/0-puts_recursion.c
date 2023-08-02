0efd12a282be6b3df0c44f629da452aabc6a77c6
michaelkingwa <kingwamaureen@gmail.com>
2023-08-02 12:54:05 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * _puts_recursion - function like puts();
- * @s: input
+ * _puts_recursion - recursive function to print to screen
+ *
+ * @s: pointer to the string
+ *
  * Return: 0
  */
 void _puts_recursion(char *s)
