f66d8fb5aaf7dc2959b69e675d7c3cce90a015e6
michaelkingwa <kingwamaureen@gmail.com>
2023-08-03 01:53:41 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -18,7 +18,7 @@ int _strlen_recursion(char *s)
  * @s: string
  * @n1: smallest iterator.
  * @n2: biggest iterator.
- * Return: 0
+ * Return: .
  */
 int comparator(char *s, int n1, int n2)
 {
@@ -29,4 +29,16 @@ int comparator(char *s, int n1, int n2)
 		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
+}
+
+/**
+ * is _ palindrome - detects if a string is a palindrome.
+ * @s: string.
+ * Return: 1 if s is a palindrome, 0 if not.
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
